cfbc1859109e8a436e7b82bb1663e6fabdb52fa9
avi-kasliwal <avi.kasliwal04@gmail.com>
2022-05-21 18:26:45 +0530

    typo fixed

--- truffle-config.js
+++ truffle-config.js
@@ -58,7 +58,7 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    rinkbey: {
+    rinkeby: {
       provider: () =>
         new HDWalletProvider(
           mnemonic,
